ed467f0fa03d453ef208c5077be3cf192fbcc67f
xue12311 <1712530035@qq.com>
2022-07-02 20:58:19 +0800

    WiFi配置

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
 #include <Arduino.h>
-void setup() { 
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include "../lib/WiFiManager/WiFiManager.h"
+
+WiFiManager wifiManager;
+
+void setup() {
+    Serial.begin(9600);
+    wifiManager.autoConnect("ESP8266 e-Paper", "12345678");
+    Serial.println("");
+    Serial.println("Esp8266 WiFi 连接成功 : " + WiFi.SSID());
+    Serial.println("IP 地址 : " + WiFi.localIP().toString());
 }
 
 void loop() {
